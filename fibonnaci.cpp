b1d0464609982825200e8045ce96297392433d4e
Ayush Bindlish <ayushbindlish96@gmail.com>
2018-03-23 23:52:45 +0530

    fibonnaci updated

--- fibonnaci.cpp
+++ fibonnaci.cpp
@@ -4,10 +4,13 @@
 //#include<bits/stdc++.h>
 using namespace std;
 int main() {
-	
+	int n1=0;
+	int n2=1;
 	for(int i=0;i<11;i++){
-		int sum=sum+i;
-		cout<<double(sum)<<endl;
+		std::cout<<n1<<endl;
+		int sum=n1+n2;
+		n1=n2;
+		n2=sum;
 	}
 	return 0;
 }
