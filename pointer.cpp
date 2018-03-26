528158611942acb27eb80ddff7a008f1715e53d2
Ayush Bindlish <ayushbindlish96@gmail.com>
2018-03-26 22:42:21 +0530

    added pointer.cpp- how to use pointer

---
+++ pointer.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdio>
+//include all c++ header files 
+//#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int i=34;
+	int *a;
+	a = &i;
+	cout<<*a<<endl;
+	return 0;
+}
